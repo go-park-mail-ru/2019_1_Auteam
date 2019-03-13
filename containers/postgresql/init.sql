0a9daa7929cc8b9d209c72e98f4a9f0db91d37c9
Michael Lozhechko <mvlozhechko@gmail.com>
2019-03-13 07:01:34 +0300

    Minor. Fix sql init script and rename folder

---
+++ containers/postgresql/init.sql
@@ -0,0 +1,16 @@
+CREATE USER docker WITH PASSWORD 'docker';
+CREATE DATABASE docker;
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+
+CREATE TABLE IF NOT EXISTS users(
+  id INT PRIMARY KEY,
+  username VARCHAR(30)  NOT NULL,
+  email VARCHAR(30)  NOT NULL,
+  password VARCHAR(120) NOT NULL,
+  pic VARCHAR(120) DEFAULT NULL,
+  lvl INTEGER DEFAULT 0,
+  score INTEGER DEFAULT 0
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS "users_username_uindex" ON users (username);
+CREATE UNIQUE INDEX IF NOT EXISTS "users_score_uindex" ON users (score);
