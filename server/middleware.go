a660bd694a8ad722d1fc5e97ec9ed01c2311d807
Egor Kislukha <e.kislukha@ekislukha.local>
2019-03-13 07:14:25 +0300

    fix cors 2

--- server/middleware.go
+++ server/middleware.go
@@ -37,7 +37,7 @@ func SetCors(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", "dev.mycodestory.ru")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
 				return
